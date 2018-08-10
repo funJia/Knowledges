5197ba96ec4d77a672a060bd64d064a4c2a91bf6
funjia <743087795@qq.com>
2018-08-10 16:11:31 +0800

    Add ant+ typescript+sharedWorker 实现共享websocket

--- node+sw+ws/io/server.js
+++ node+sw+ws/io/server.js
@@ -2,10 +2,13 @@ const WebSocket = require("ws");
 
 const wss = new WebSocket.Server({ port: 8083 });
 
+// 监听连接
 wss.on("connection", function connection(ws) {
+  // 监听客户端发过来的消息
   ws.on("message", function incoming(message) {
     console.log("received: %s", message);
   });
 
+  // 发送消息到客户端
   ws.send("something");
 });
