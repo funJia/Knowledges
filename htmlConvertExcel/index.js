80d9d2ddb4f2a5df3c55f5a477056b8a2c91c5a9
funjia <743087795@qq.com>
2018-08-20 17:23:23 +0800

    Add html转换成Excel表格

---
+++ htmlConvertExcel/index.js
@@ -0,0 +1,63 @@
+const fs = require("fs");
+const htmlTo = require("html2xlsx");
+
+htmlTo(
+  `
+  <style type="text/css">
+    table td {
+      color: #666;
+      height: 20px;
+      background-color: #f1f1f1;
+      border: 1px solid #eee;
+    }
+    tr{      
+      height:27pt;
+    }
+    th{
+      height:27pt;
+      text-align: center;
+      color: windowtext;
+      font-size: 22.0pt;
+      font-weight: 700;
+      font-family: 黑体, monospace;
+    }
+  </style>
+  <table>
+    <tr>
+      <th colspan="2">中心</th>      
+    </tr>
+    <tr>
+      <td>hello</td>
+      <td>world</td>
+    </tr>
+    <tr>
+      <td type="number">123</td>
+      <td type="number">123.456</td>
+    </tr>
+    <tr>
+      <td data-type="bool">true</td>
+      <td data-type="bool">false</td>
+    </tr>
+    <tr>
+      <td data-type="bool">1</td>
+      <td data-type="bool">0</td>
+    </tr>
+    <tr>
+      <td type="formula">SUM(A1:B1)</td>
+      <td type="formula">A1-B1</td>
+    </tr>
+    <tr>
+      <td type="date">2013-01-12T12:34:56+08:00</td>
+      <td type="datetime">2013-01-12T12:34:56+08:00</td>
+    </tr>
+  </table>
+`,
+  (err, file) => {
+    if (err) return console.error(err);
+
+    file
+      .saveAs()
+      .pipe(fs.createWriteStream("test13.xlsx"))
+      .on("finish", () => console.log("Done."));
+  }
+);
---
+++ htmlConvertExcel/readme.md
@@ -0,0 +1,15 @@
+2018/08/20 17:17:39
+
+html 转 xlsx
+
+https://github.com/d-band/html2xlsx
+
+2018/08/20 17:19:06
+
+找到的而外信息
+
+安装时跳过安装 chrome
+https://lzw.me/a/puppeteer-install-skip-download-chrome.html
+
+chrome 开发版
+https://free-vpn.github.io/chrome/
